3c54d3d9408b44e33bb4019972f21560dd2045c8
Minhazrayeem7 <109741594+Minhazrayeem7@users.noreply.github.com>
2022-08-14 07:22:37 +0600

    Add files via upload

---
+++ B.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i;
+    long long ll;
+    char ch;
+    float f;
+    double lf;
+    cin>>i>>ll>>ch>>f>>lf;
+    cout<<i<<"\n";
+    cout<<ll<<"\n";
+    cout<<ch<<"\n";
+    cout<<f<<"\n";
+    cout<<lf<<"\n";
+    return 0;
+}
+
---
+++ E.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+ double R;
+ double pi=3.141592653;
+ cin>>R;
+ cout<<fixed<<setprecision(9)<<pi*(R*R)<<"\n";
+ return 0;
+
+}
